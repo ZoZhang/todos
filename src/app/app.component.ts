66ad8f9eb748a02cdc8e960b6ecd7aaa6151d357
Zhao ZHANG <zo.zhang@gmail.com>
2020-12-13 04:03:54 +0100

    Adding hotkeys service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {TodoService} from './services/todo.service';
+import {Hotkeys} from './services/hotkeys.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,23 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(private hotkeys: Hotkeys, private todoService: TodoService) {}
+
+  ngOnInit() {
+    // Undo
+    this.hotkeys.addShortcut({ keys: 'control.z' }).subscribe(this.undo);
+
+    // Redo
+    this.hotkeys.addShortcut({ keys: 'control.shift.z' }).subscribe(this.redo);
+  }
+
+  undo($event) {
+
+  }
+
+  redo($event) {
+
+  }
+}
